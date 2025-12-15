2e8617bc0b8315aa68a80b6b0a2dced4e8e8ea3c
miguel-merlin <mmerlin@stevens.edu>
2025-12-15 12:22:26 -0600

    Initial commit

---
+++ test/crm-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrmInfra from '../lib/crm-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/crm-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrmInfra.CrmInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
